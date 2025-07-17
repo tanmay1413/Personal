6d5f3e9e4eaf9caf4a9b5dd7640d424b606db86e
tanmay1413 <tanmaykulkarni1413@gmail.com>
2025-07-17 23:25:40 +0530

    if else elif

--- Recap/Day1.py
+++ Recap/Day1.py
@@ -28,7 +28,7 @@ name = "Amol"
 print("type of name:",type(name))
 
 # declaration of variabel 
-# variabel are always start with  letters 
+# variabel are always start with  letters  
 # variabel are dose not start with number and special char
 #
 one_1 = 123
@@ -116,3 +116,132 @@ print("Floor division = ", f)
 # Exponent (**)
 g = 5 ** 2
 print("Exponent = ", g)
+
+
+## ---Assignment operator----
+# 1) equal (=)
+name = "Ajay"
+
+# ( += )
+x = 10
+x += 1
+print(x)
+
+# ( -= )
+x = 10
+x -= 1
+print(x)
+
+# ( *= )
+x = 10
+x *= 10
+print(x)
+
+# ( /= )
+x = 10
+x /= 10
+print(x)
+
+# ( //= )
+x = 10
+x //= 7
+print(x)
+
+# ( %= )
+x = 10
+x %= 7
+print(x)
+
+# ( **= )
+x = 10
+x **= 10
+print(x)
+
+# logical operator 
+# 1) and  ---> both conditions are true 
+# 2) or   ---> At least one condition is True
+# 3) not  ---> Reverse condtion 
+
+a , b = 10 , 21
+if a == 10 or b == 20 :
+  print("done")
+else:
+  print("Faild")
+
+
+# Membership operator 
+# 1) in
+# 2) not in
+
+li = [10, 20 ,30]
+if 30 not in li:
+  print("True")
+else:
+  print("False")
+
+
+# Identity operator 
+#  1) is 
+#  2) not is 
+
+a = [1, 2, 3]
+b = a   
+
+if a is not b:
+    print("a is b")     
+else:
+    print("a is not b")
+
+
+
+# Condition statements 
+# if , elif , else
+
+# Q1) check the marks of the student and give the result 
+#  condition > 35 pass
+# < 35 fail
+# > 80 first class 
+"""
+number = int(input("enter the mark :"))
+
+if number < 0 :
+   print("enter Correct marks!!!!")
+elif number > 80:
+    print("First class")
+elif number > 35 and number < 80:
+    print("Pass")
+else:
+    print("fail")
+"""
+
+# Q2) If height is grater than 160 cm and age is grater than 18 print selected other wise not selected 
+
+"""
+height =  int(input("Enter height :"))
+age =  int(input("Enter age :"))
+
+if age >= 18 and height >= 160:
+   print("Selected ....")
+else:
+   print("Not Selected...")
+
+"""
+
+# Q3) write a program to calculate the electrcity bill take input from user for number of unit used 
+"""
+    Unit            Price 
+first 100 units      Free(Rs.0)
+next 100 units      Rs. 5/unit
+after 200 units     Rs. 10/unit
+""" 
+
+unit = int(input("enter the unit :"))
+bill = 0
+if unit >= 0 and unit <= 100 :
+    bill = 0
+elif unit > 100 and unit <= 200:
+    bill = unit * 5
+else:
+    bill = unit * 10
+
+print(f"The bill of {unit} is {bill}")
